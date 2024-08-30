7234435cda671a2032bbc69f9d6f4648042dfbdf
perle-alkhodari <perle.alkhodari@gmail.com>
2024-08-30 03:57:32 +0300

    Made lofi keys. pretty sounds.

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ var drumButtons = document.querySelectorAll(".drum");
 for (var i = 0; i < drumButtons.length; i++) {
     drumButtons[i].addEventListener("click", function() {
         var audio = new Audio(`drum-sounds/${this.innerHTML}.mp3`);
+        audio.volume = 0.5;
         audio.play();
     });
     document.addEventListener("keydown", function(event) {
         var audio = new Audio(`drum-sounds/${event.key}.mp3`);
+        audio.volume = 0.5;
         audio.play();
     })
 }
@@ -23,6 +25,21 @@ for (var i = 0; i < guitarButtons.length; i++) {
     document.addEventListener("keydown", function(event) {
         var audio = new Audio(`guitar-sounds/${event.key}.mp3`);
         audio.volume = 0.25;
+        audio.play();
+    })
+}
+
+// lofi buttons
+var lofiButtons = document.querySelectorAll(".lofi");
+for (var i = 0; i < lofiButtons.length; i++) {
+    lofiButtons[i].addEventListener("click", function() {
+        var audio = new Audio(`lofi-sounds/${this.innerHTML}.mp3`);
+        audio.volume = 0.8;
+        audio.play();
+    });
+    document.addEventListener("keydown", function(event) {
+        var audio = new Audio(`lofi-sounds/${event.key}.mp3`);
+        audio.volume = 0.8;
         audio.play();
     })
 }
